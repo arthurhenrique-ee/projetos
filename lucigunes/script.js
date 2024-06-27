c0fc8b5d099d4dec43532026a4777633ce39c0f6
Arthur Henrique Gunes <art123.gunesmarciano@gmail.com>
2024-06-27 14:59:25 -0300

    Mais estilizações
    Ta ficando Toppppp

--- lucigunes/script.js
+++ lucigunes/script.js
@@ -31,3 +31,30 @@ function lerMais() {
         texto.style.display = 'none'
     }
 }
+
+document.addEventListener('scroll', function() {
+    const header = document.getElementById('header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
+function toggleAnswer(question) {
+    // Encontra o elemento pai da pergunta (<li>)
+    let listItem = question.parentNode.parentNode;
+    
+    // Encontra o elemento resposta dentro do <li> atual
+    let answer = listItem.querySelector('.answer');
+
+    // Obtém a altura atual da resposta
+    let height = answer.clientHeight;
+
+    // Alterna a altura da resposta
+    if (height === 0) {
+        answer.style.height = answer.scrollHeight + 'px'; // Define a altura para a altura total da resposta
+    } else {
+        answer.style.height = '0'; // Define a altura de volta para zero
+    }
+}
