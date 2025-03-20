4f46ca99a85e4fbbaabd1ee0184a6d5493e63e1e
Arthur Henrique Gunes <art123.gunesmarciano@gmail.com>
2025-03-20 13:47:40 -0300

    Responsividade e correção de erros

--- bodysoulfitness/scripts/cronograma.js
+++ bodysoulfitness/scripts/cronograma.js
@@ -1,13 +1,11 @@
 let dias = document.querySelectorAll('.day');
 let contents = document.querySelectorAll('.content');
 
-let contentDom = document.querySelector('.content-dom')
 let contentSeg = document.querySelector('.content-seg')
 let contentTer = document.querySelector('.content-ter')
 let contentQua = document.querySelector('.content-qua')
 let contentQui = document.querySelector('.content-qui')
 let contentSex = document.querySelector('.content-sex')
-let contentSab = document.querySelector('.content-sab')
 
 contents.forEach(content => {
     if (!content.classList.contains('content-seg')) {
@@ -21,33 +19,27 @@ dias.forEach(dia => {
         dia.classList.add('checked');
 
         contents.forEach(content => content.style.display = 'none');
-        
-        if (dia.classList.contains('dom')) {
-            contentDom.style.display = 'block'
+
+        if (dia.classList.contains('seg')) {
+            contentSeg.style.display = 'block'
         } else {
-            if (dia.classList.contains('seg')) {
-                contentSeg.style.display = 'block'
+            if (dia.classList.contains('ter')) {
+                contentTer.style.display = 'block'
             } else {
-                if (dia.classList.contains('ter')) {
-                    contentTer.style.display = 'block'
+                if (dia.classList.contains('qua')) {
+                    contentQua.style.display = 'block'
                 } else {
-                    if (dia.classList.contains('qua')) {
-                        contentQua.style.display = 'block'
+                    if (dia.classList.contains('qui')) {
+                        contentQui.style.display = 'block'
                     } else {
-                        if (dia.classList.contains('qui')) {
-                            contentQui.style.display = 'block'
-                        } else {
-                            if (dia.classList.contains ('sex')) {
-                                contentSex.style.display = 'block'
-                            } else {
-                                if (dia.classList.contains('sab')) {
-                                    contentSab.style.display = 'block'                                
-                                }
-                            }
+                        if (dia.classList.contains('sex')) {
+                            contentSex.style.display = 'block'
                         }
+
                     }
                 }
             }
         }
-    });
+    }
+    )
 });
