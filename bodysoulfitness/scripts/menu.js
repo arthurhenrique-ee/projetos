bf54984e656b222961b70459a3202051ed4ebd23
Arthur Henrique Gunes <art123.gunesmarciano@gmail.com>
2025-03-19 22:03:06 -0300

    Menu Mobile + Seção Equipe

--- bodysoulfitness/scripts/menu.js
+++ bodysoulfitness/scripts/menu.js
@@ -1,4 +1,9 @@
+let body = document.querySelector('#body')
 let header = document.querySelector('#header')
+let btnMenuMobile = document.querySelector('.btn-menu')
+let btnCloseMenu = document.querySelector('.btn-close-menu')
+let menuMobile = document.querySelector('.menu-mobile')
+let linksMenuMobile = document.querySelectorAll('.link-menu-mobile')
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > 0) {
@@ -6,4 +11,20 @@ window.addEventListener('scroll', () => {
     } else {
         header.classList.remove('ativo')
     }
+})
+
+btnMenuMobile.addEventListener('click', () => {
+    menuMobile.classList.toggle('active')
+    body.classList.toggle('no-scroll')
+})
+
+btnCloseMenu.addEventListener('click', () => {
+    menuMobile.classList.remove('active')
+    body.classList.remove('no-scroll')
+})
+
+linksMenuMobile.forEach(link => {
+    link.addEventListener('click', () => {
+        menuMobile.classList.remove('active')
+    })
 })
